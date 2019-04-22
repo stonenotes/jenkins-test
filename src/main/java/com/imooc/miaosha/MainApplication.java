7875147d1ea52abc4b4faa4398121a13678d1158
stonenotes <stonenotes@sina.cn>
2019-04-22 22:37:52 +0800

    提交war配置

--- src/main/java/com/imooc/miaosha/MainApplication.java
+++ src/main/java/com/imooc/miaosha/MainApplication.java
@@ -2,12 +2,18 @@ package com.imooc.miaosha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
-	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MainApplication.class);
+	}
 }
